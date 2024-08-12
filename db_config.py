26c5dcbc6c7ea4c059b450259ddb49b799149ea6
leonardo-feitas-dev <leonardo_freitas07@outlook.com>
2024-08-12 08:29:43 -0300

    alteração no db_config.py

--- db_config.py
+++ db_config.py
@@ -1,14 +1,11 @@
 import psycopg2
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
 def get_db_connection():
     conn = psycopg2.connect(
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT'),
-        database=os.getenv('DB_NAME')
+        user= ('DB_USER'),
+        password= ('DB_PASSWORD'),
+        host= ('DB_HOST'),
+        port= ('DB_PORT'),
+        database= ('DB_NAME')
     )
     return conn
